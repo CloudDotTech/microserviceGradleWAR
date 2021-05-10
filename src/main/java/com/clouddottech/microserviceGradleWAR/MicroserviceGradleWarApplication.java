f803cef4e8be8251646ec1a055e758c853b88cde
First Last <email.mail.com>
2021-05-10 01:17:42 -0500

    first commit

---
+++ src/main/java/com/clouddottech/microserviceGradleWAR/MicroserviceGradleWarApplication.java
@@ -0,0 +1,13 @@
+package com.clouddottech.microserviceGradleWAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceGradleWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceGradleWarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clouddottech/microserviceGradleWAR/MicroserviceGradleWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clouddottech.microserviceGradleWAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceGradleWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
