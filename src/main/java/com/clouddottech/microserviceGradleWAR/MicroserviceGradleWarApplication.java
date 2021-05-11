c3baec5f5060895dba5dbca476387884276436b0
First Last <email.mail.com>
2021-05-10 23:13:48 -0500

    first commit 1

--- src/main/java/com/clouddottech/microserviceGradleWAR/MicroserviceGradleWarApplication.java
+++ src/main/java/com/clouddottech/microserviceGradleWAR/MicroserviceGradleWarApplication.java
@@ -2,12 +2,19 @@ package com.clouddottech.microserviceGradleWAR;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MicroserviceGradleWarApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceGradleWarApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceGradleWarApplication.class, args);
+    }
 
+    @RequestMapping(value = "/")
+    public String hello() {
+        return "Hello Microservice Gradle WAR!";
+    }
 }
